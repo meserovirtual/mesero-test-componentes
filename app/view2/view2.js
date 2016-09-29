6e3bb2e6375879d4bf4793eb60599477183391f8
meserovirtual <meserovirtualarg@gmail.com>
2016-09-29 20:01:27 -0300

    Mateo
    Terminar de dar formato a la vista

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -8,86 +8,10 @@
       .controller('View2Ctrl', View2Ctrl);
 
 
-  View2Ctrl.$inject = ['$scope', 'UserVars', 'UserService', 'AcUtils'];
-  function View2Ctrl($scope, UserVars, UserService, AcUtils) {
+  View2Ctrl.$inject = ['$scope'];
+  function View2Ctrl($scope) {
 
     var vm = this;
-
-    vm.usuarios = [];
-    vm.usuario = {};
-    vm.soloActivos = false;
-
-
-    vm.setData = setData;
-    vm.loadUsuarios = loadUsuarios;
-
-    function loadUsuarios() {
-      console.log(vm.soloActivos);
-      if(!vm.soloActivos){
-        UserVars.all = false;
-        UserService.get().then(function (data) {
-          setData(data);
-        });
-      } else {
-        UserVars.all = true;
-        UserService.get().then(function (data) {
-          setData(data);
-        });
-      }
-    }
-
-    function setData(data) {
-      vm.usuarios = data;
-      vm.paginas = UserVars.paginas;
-      console.log(vm.usuarios);
-    }
-
-    UserService.get().then(function (data) {
-      setData(data);
-    });
-
-    function save() {
-      if (vm.usuario.usuario_id == undefined) {
-        vm.usuario.status = 1;
-      }
-      UserService.save(vm.usuario).then(function (data) {
-        return UserService.get();
-      }).then(function (data) {
-        setData(data);
-      });
-
-    }
-
-    // Implementación de la paginación
-    vm.start = 0;
-    vm.limit = UserVars.paginacion;
-    vm.pagina = UserVars.pagina;
-    vm.paginas = UserVars.paginas;
-
-    function paginar(vars) {
-      if (vars == {}) {
-        return;
-      }
-      vm.start = vars.start;
-      vm.pagina = vars.pagina;
-    }
-
-    vm.next = function () {
-      paginar(AcUtils.next(UserVars));
-    };
-    vm.prev = function () {
-      paginar(AcUtils.prev(UserVars));
-    };
-    vm.first = function () {
-      paginar(AcUtils.first(UserVars));
-    };
-    vm.last = function () {
-      paginar(AcUtils.last(UserVars));
-    };
-
-    vm.goToPagina = function () {
-      paginar(AcUtils.goToPagina(vm.pagina, UserVars));
-    }
 
   }
 
